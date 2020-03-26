0ee29bc6ea7c7df03cb00dff1afe79fddbf70201
Wiking <p.polcik1@gmail.com>
2020-03-26 08:32:48 +0100

    Added project template

---
+++ sendAck.h
@@ -0,0 +1,22 @@
+/**
+ *  @author Luca Pietro Borsani
+ */
+
+#ifndef SENDACK_H
+#define SENDACK_H
+
+//payload of the msg
+typedef nx_struct my_msg {
+	//field 1
+	//field 2
+	//field 3
+} my_msg_t;
+
+#define REQ 1
+#define RESP 2 
+
+enum{
+AM_MY_MSG = 6,
+};
+
+#endif
